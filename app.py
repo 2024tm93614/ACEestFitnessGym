d919d1b44012c7c6d6a6c1b0c185a71132fb9241
anshu <2024tm93614@wilp.bits-pilani.ac.in>
2026-03-09 19:09:19 +0530

    Version 1.0 - Basic Flask API with init, add, members endpoints

--- app.py
+++ app.py
@@ -2,13 +2,33 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# Fake database
+members = []
+
 @app.route("/")
 def home():
-    return jsonify({
-        "app": "ACEest Fitness Gym",
-        "version": "1.0",
-        "message": "Welcome to ACEest Fitness Gym API"
-    })
+    return jsonify({"message": "Welcome to ACEest Fitness & Gym API"})
+
+
+@app.route("/init")
+def init_database():
+    global members
+    members = []
+    return jsonify({"message": "Database initialized"})
+
+
+@app.route("/add")
+def add_member():
+    global members
+    new_member = {"name": "Rahul", "plan": "Premium"}
+    members.append(new_member)
+    return jsonify({"message": "Member added", "member": new_member})
+
+
+@app.route("/members")
+def get_members():
+    return jsonify(members)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
